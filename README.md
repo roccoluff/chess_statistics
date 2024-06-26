793447ec726aa352b82c4ae2015efad0a0e3bb01
Rocco Luffarelli <46350594+roccoluff@users.noreply.github.com>
2024-06-26 15:12:18 +0200

    Backend fetches pgn of users in file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # chess_statistics
+
--- main.py
+++ main.py
@@ -1,27 +1,39 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 import requests
 
 app = FastAPI()
 
-# Valid title abbreviations
+@app.get("/monthly/{monthly_pgn}")
+def get_monthly_png(username: str, year: int, month: int):
 
-@app.get("/titled/{title}")
-def get_players_info(title: str):
-
-    url = f"https://api.chess.com/pub/titled/{title}"
+    url = f"https://api.chess.com/pub/player/{username}/games/{year}/{month}/pgn"
     headers = {
-        "User-Agent": "YourAppName/1.0"
+        "User-Agent": "chess_statistics/0.1",
     }
     response = requests.get(url, headers=headers)
 
     # Check for successful response
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail="Error fetching data from Chess.com")
+    
 
-    data = response.json()
-    return data
+    # This extracts and puts the response in a text format
+    pgn_content = response.text
+
+    filename = f"ChessCom_{username}_{year}_{str(month).zfill(2)}.pgn"
+
+     # Create and return the response with appropriate headers
+    return Response(
+        content=pgn_content,
+        media_type="application/x-chess-pgn",
+        headers={
+            "Content-Disposition": f"attachment; filename={filename}"
+        }
+    )
 
 # Run the app using uvicorn
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
